ed48e6d822f0026ad4cab68da44960dce07fc2e1
Israel Oluwasegun <marksegman@outlook.com>
2025-06-27 09:21:50 +0100

    add MySQLServer.py

---
+++ MySQLServer.py
@@ -0,0 +1,26 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+try:
+    connection = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "Makinde0604",
+        database = "alx_book_store"
+    )
+
+except Exception as e:
+    print("Unable to connect")
+else:
+    print("connected sucessfully")
+    try:
+        cursor = connection.cursor()
+        cursor.execute("""CREATE DATABAE alx_book_store""")
+        print("Database 'alx_book_store' created successfully!")
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_DB_CREATE_EXISTS:
+            print("Database 'alx_book_store' already exists.")
+        else:
+            print("Error",err)
+
+
